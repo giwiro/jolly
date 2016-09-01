6bf1e24d1d97f0d4fe220e9a1a52d2a5c63e95fa
Gi Wah <giwirodavalos@gmail.com>
2016-09-01 01:02:02 -0500

    Now the DBManager can perform querys in a better way. "Inspired" on: https://github.com/neo4j-examples/neo4j-movies-java-bolt/blob/master/src/main/java/example/movies/executor/BoltCypherExecutor.java

--- src/main/java/app/db/DBManager.java
+++ src/main/java/app/db/DBManager.java
@@ -3,6 +3,10 @@ package app.db;
 import app.Conf;
 import org.neo4j.driver.v1.*;
 
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by Gi Wah Davalos on 28/08/2016.
  */
@@ -21,7 +25,6 @@ public class DBManager {
 
         initConstraint(driver);
 
-
     }
 
     public static DBManager getInstance () {
@@ -31,13 +34,33 @@ public class DBManager {
         return dbManager;
     }
 
+    public Iterator<Map<String, Object>> query(String query, Map<String, Object> params) {
+        try (Session session = driver.session()) {
+            List<Map<String, Object>> list = session.run(query, params)
+                    .list( r -> r.asMap(DBManager::convert));
+            return list.iterator();
+        }
+    }
+
+    static Object convert(Value value) {
+        switch (value.type().name()) {
+            case "PATH":
+                return value.asList(DBManager::convert);
+            case "NODE":
+            case "RELATIONSHIP":
+                return value.asMap();
+        }
+        return value.asObject();
+    }
+
     private static void initConstraint(Driver driver) {
         Session session = driver.session();
-        final String q = "CREATE CONSTRAINT ON (producto:Producto) ASSERT producto.id IS UNIQUE";
+        final String q1 = "CREATE CONSTRAINT ON (producto:Producto) ASSERT producto.id IS UNIQUE";
+        final String q2 = "CREATE CONSTRAINT ON (categoria:Categoria) ASSERT categoria.id IS UNIQUE";
 
+        session.run(q1);
+        session.run(q2);
 
-
-        session.close();
     }
 
     public Driver getDriver () {
